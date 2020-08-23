0d244473d3d35bc369cfc34e80ec9caa92587931
Nathan-Payne <paynee89@yahoo.co.uk>
2020-08-23 01:42:14 +0100

    add binance websocket connection after ticker component mount

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="white--text c-3">ticker</div>
+  <div class="white--text c-3" @click="getTickers">ticker {{ btcTicker }}</div>
 </template>
 
 <script>
 export default {
-  name: "Ticker"
+  name: "Ticker",
+  data() {
+    return {
+      btcTicker: "fetching..."
+    };
+  },
+  methods: {
+    getTickers() {
+      //https://binance-docs.github.io/apidocs/spot/en/#individual-symbol-ticker-streams for data format
+      const socket = new WebSocket(
+        "wss://stream.binance.com:9443/ws/btcusdt@ticker"
+      );
+      socket.onmessage = event => {
+        const parsedData = JSON.parse(event.data);
+        const lastPrice = parseFloat(parsedData.c).toFixed(2);
+        this.btcTicker = lastPrice;
+      };
+    }
+  },
+  mounted() {
+    this.getTickers();
+  }
 };
 </script>
 
